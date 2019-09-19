4603627c86292c9b544de517655ade49bcfc240c
Aayushi <55234159+Mishti08@users.noreply.github.com>
2019-09-19 20:23:09 +0530

    Create Datatype_combination.py
    Comment regarding todo list

---
+++ Datatype_combination.py
@@ -0,0 +1,11 @@
+"""
+Python combinations of datatype
+1.List inside Dictionary  {key:[values]}
+2.Dictionary inside List  [value,{key:value}]
+3.List inside set    {[list1],[list2]}
+4.Set inside list    [{set1},{set2}]
+5.List inside tuple  ([list1],[list2])
+6.Tuple inside list  [(val1,val2),(val3,val4)]
+7.Tuple inside Dictionary  {key:(values)}
+and so many other combinations based on the need we can use the combination of datatypes
+"""
